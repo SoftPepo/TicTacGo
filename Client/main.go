3a67cafcb823e5ec3a23e0978ccbad6d3e83fbdd
Kacper <kacper.paracki2004@gmail.com>
2026-08-18 16:10:41 +0200

    Add command selection in client

--- Client/main.go
+++ Client/main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/coder/websocket"
 )
@@ -15,7 +16,7 @@ type ClientMsg struct {
 	nick     string
 	roomName string
 	password string
-	cell     int
+	cell     string
 }
 
 type Client struct {
@@ -30,6 +31,9 @@ const (
 	Hello  Kind = "Hello"
 	Create Kind = "Create"
 	Join   Kind = "Join"
+	Move   Kind = "Move"
+	Leave  Kind = "Leave"
+	Exit   Kind = "Exit"
 )
 
 func main() {
@@ -58,7 +62,44 @@ func main() {
 	}()
 
 	for scanner.Scan() {
-		msg, _ := json.Marshal(ClientMsg{kind: Create, roomName: "testroom", password: "1234", cell: 1})
-		conn.Write(ctx, websocket.MessageText, []byte(msg))
+		var msg ClientMsg
+		input := strings.Fields(scanner.Text())
+		switch input[0] {
+		case "Create":
+			if len(input) != 3 {
+				fmt.Println("Invalid format, Create <Room> <Password>")
+				continue
+			}
+			msg = ClientMsg{kind: Create, roomName: input[1], password: input[2]}
+		case "Join":
+			if len(input) != 3 {
+				fmt.Println("Invalid format, Join <Room> <Password>")
+				continue
+			}
+			msg = ClientMsg{kind: Join, roomName: input[1], password: input[2]}
+		case "Move":
+			if len(input) != 3 {
+				fmt.Println("Invalid format, Move <Cell>")
+				continue
+			}
+			msg = ClientMsg{kind: Move, cell: input[1]}
+		case "Leave":
+			if len(input) != 1 {
+				fmt.Println("Leave command takes no parameters")
+				continue
+			}
+			msg = ClientMsg{kind: Leave}
+		case "Exit":
+			if len(input) != 1 {
+				fmt.Println("Exit command takes no parameters")
+				continue
+			}
+			msg = ClientMsg{kind: Exit}
+		default:
+			fmt.Println("Unknown command")
+			continue
+		}
+		data, _ := json.Marshal(msg)
+		conn.Write(ctx, websocket.MessageText, []byte(data))
 	}
 }
