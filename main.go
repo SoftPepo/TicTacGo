758917091ed13a6513ffedbcd2613469b169e01e
Kacper <kacper.paracki2004@gmail.com>
2026-08-24 10:52:54 +0200

    feat: handle move in server readpump feat: convert json to ack in client readpump fix: empty user input no longer panics fix: board froze on proper move

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log/slog"
 	"net/http"
 
@@ -33,10 +34,10 @@ type Board struct {
 }
 
 type GameState struct {
-	boardState    [9]Cell
-	currentPlayer string
-	status        Status
-	result        Result
+	boardState [9]Cell
+	currentIdx int
+	status     Status
+	result     Cell
 }
 
 type Command struct {
@@ -76,38 +77,30 @@ type GameCommand struct {
 	kind   GameCommandKind
 	client *Client
 	cell   int
+	reply  chan Response
 }
 
 type GameCommandKind string
 
 const (
-	Move  GameCommandKind = "Move"
-	Leave GameCommandKind = "Leave"
+	Move      GameCommandKind = "Move"
+	Leave     GameCommandKind = "Leave"
+	AddPlayer GameCommandKind = "AddPlayer"
 )
 
 type Status string
 
 const (
-	Ongoing  Status = "Ongoing"
 	Finished Status = "Finished"
 	Aborted  Status = "Aborted"
-)
-
-type Result string
-
-const (
-	A         Result = "A"
-	B         Result = "B"
-	Stalemate Result = "Stalemate"
-	None      Result = "None"
 )
 
 type Cell string
 
 const (
-	Empty   Cell = "E"
-	PlayerA Cell = "A"
-	PlayerB Cell = "B"
+	Empty Cell = ""
+	X     Cell = "X"
+	O     Cell = "O"
 )
 
 type ClientMsg struct {
@@ -117,6 +110,14 @@ type ClientMsg struct {
 	Password string `json:"password"`
 	Cell     int    `json:"cell"`
 }
+
+var winLines = [8][3]int{
+	{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+	{0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+	{0, 4, 8}, {2, 4, 6},
+}
+
+var symbols = [2]Cell{X, O}
 
 func (c *Client) readPump(ctx context.Context) {
 	defer func() { c.hub.command <- Command{kind: Unregister, client: c} }()
@@ -137,6 +138,8 @@ func (c *Client) readPump(ctx context.Context) {
 			if !resp.ok {
 				ack, _ := json.Marshal(Ack{Kind: "Ack", Ok: false, Code: resp.code, Msg: resp.msg})
 				c.send <- ack
+			} else {
+				c.board = resp.board
 			}
 		case "Move":
 			if c.board == nil {
@@ -144,7 +147,7 @@ func (c *Client) readPump(ctx context.Context) {
 				c.send <- ack
 			}
 			reply := make(chan Response, 1)
-			gamecmd := GameCommand{kind: Move, client: c, cell: msg.Cell}
+			gamecmd := GameCommand{kind: Move, client: c, cell: msg.Cell, reply: reply}
 			c.board.command <- gamecmd
 			resp := <-reply
 			if !resp.ok {
@@ -168,6 +171,7 @@ func (c *Client) writePump(ctx context.Context) {
 func (h *Hub) run() {
 	for {
 		cmd := <-h.command
+		slog.Info("Command received ", "kind", cmd.kind)
 		switch cmd.kind {
 		case Register:
 			h.clients[cmd.client] = true
@@ -175,17 +179,28 @@ func (h *Hub) run() {
 		case Unregister:
 			delete(h.clients, cmd.client)
 		case Create:
+			if h.playerInGame(cmd.client) {
+				cmd.reply <- Response{ok: false, code: "player_in_game", msg: "Player already in game"}
+				continue
+			}
 			if _, ok := h.boards[cmd.name]; !ok {
 				h.boards[cmd.name] = &Board{
 					name:     cmd.name,
 					password: cmd.password,
+					command:  make(chan GameCommand, 16),
 					done:     make(chan struct{}),
 				}
-				h.boards[cmd.name].players[0] = cmd.client
-				cmd.reply <- Response{ok: true, board: h.boards[cmd.name]}
+				go h.boards[cmd.name].run()
+				h.boards[cmd.name].command <- GameCommand{kind: AddPlayer, client: cmd.client, reply: cmd.reply}
 				slog.Info("Room created", "name", cmd.name)
+			} else {
+				cmd.reply <- Response{ok: false, code: "room_name_exists", msg: "Room with this name already exists"}
 			}
+
 		case Join:
+			if h.playerInGame(cmd.client) {
+				cmd.reply <- Response{ok: false, code: "player_in_game", msg: "Player already in game"}
+			}
 			if board, ok := h.boards[cmd.name]; !ok {
 				cmd.reply <- Response{ok: false, code: "no_such_room", msg: "This room does not exist"}
 			} else if board.password != cmd.password {
@@ -202,6 +217,82 @@ func (h *Hub) run() {
 			}
 		}
 	}
+}
+
+func (h *Hub) playerInGame(c *Client) bool {
+	for _, board := range h.boards {
+		if board.players[0] == c || board.players[1] == c {
+			return true
+		}
+	}
+	return false
+}
+
+func (b *Board) run() {
+	for {
+		cmd := <-b.command
+		slog.Info("Received game command ", "kind", cmd.kind)
+		switch cmd.kind {
+		case "Move":
+			if b.players[1] == nil {
+				cmd.reply <- Response{ok: false, code: "game_not_started", msg: "Game has not yet started"}
+				continue
+			}
+			if cmd.cell < 0 || cmd.cell > 8 {
+				cmd.reply <- Response{ok: false, code: "invalid_cell", msg: "Cell our of range (0-8)"}
+				continue
+			}
+			if b.indexOf(cmd.client) != b.gameState.currentIdx {
+				cmd.reply <- Response{ok: false, code: "invalid_turn", msg: "This is not your turn"}
+				continue
+			}
+			if b.gameState.boardState[cmd.cell] != "" {
+				cmd.reply <- Response{ok: false, code: "invalid_cell", msg: "This cell is not empty"}
+				continue
+			}
+			slog.Info("Making move", "Current inx", b.gameState.currentIdx, "Symbol", symbols[b.indexOf(cmd.client)])
+			b.gameState.boardState[cmd.cell] = symbols[b.indexOf(cmd.client)]
+			fmt.Println(b.checkWinner())
+			if b.checkWinner() != Empty {
+				b.gameState.result = b.checkWinner()
+				cmd.reply <- Response{ok: false, code: "win", msg: "Game won"}
+			}
+			b.gameState.currentIdx = 1 - b.gameState.currentIdx
+			cmd.reply <- Response{ok: true}
+		case "AddPlayer":
+			if b.players[0] == nil {
+				b.players[0] = cmd.client
+				cmd.reply <- Response{ok: true, board: b}
+			} else if b.players[1] == nil {
+				b.players[1] = cmd.client
+				cmd.reply <- Response{ok: true, board: b}
+			} else {
+				cmd.reply <- Response{ok: false, code: "room_full", msg: "Room you are  trying to join is full"}
+			}
+		}
+	}
+}
+
+func (b *Board) indexOf(c *Client) int {
+	if b.players[0] == c {
+		return 0
+	} else if b.players[1] == c {
+		return 1
+	} else {
+		return -1
+	}
+}
+
+func (b *Board) checkWinner() Cell {
+	for _, line := range winLines {
+		x := b.gameState.boardState[line[0]]
+		y := b.gameState.boardState[line[1]]
+		z := b.gameState.boardState[line[2]]
+		if x != Empty && x == y && y == z {
+			return x
+		}
+	}
+	return Empty
 }
 
 func handleWS(hub *Hub, w http.ResponseWriter, r *http.Request) {
